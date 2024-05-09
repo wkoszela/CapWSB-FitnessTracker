5b73ab1e54c59badc5466fba94c8d3763b35e584
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-05-09 18:32:41 +0200

    Lab - Spring AOP: @After + Pointcut @GetMapping

--- src/main/java/com/capgemini/wsb/fitnesstracker/livecoding/aop/LoggingAspect.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/livecoding/aop/LoggingAspect.java
@@ -1,8 +1,10 @@
 package com.capgemini.wsb.fitnesstracker.livecoding.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,4 +17,24 @@ public class LoggingAspect {
         log.info("Do something before getName is called");
     }
 
+    @After("execution(public String getName())")
+    public void doSomethingAfterGetName() {
+        log.info("Do something after getName is called");
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMappingPointcut() {
+        // this is a Pointcut for all methods annotated with @GetMapping
+    }
+
+    @Before("getMappingPointcut()")
+    public void logControllerMethod() {
+        log.info("Do something before controller method is called");
+    }
+
 }
+
+
+
+
+
--- src/main/java/com/capgemini/wsb/fitnesstracker/livecoding/aop/TestController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/livecoding/aop/TestController.java
@@ -1,11 +1,11 @@
 package com.capgemini.wsb.fitnesstracker.livecoding.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/v1/test")
 @Slf4j
 public class TestController {
@@ -14,6 +14,12 @@ public class TestController {
     public String getName() {
         log.info("getName() called");
         return "John";
+    }
+
+    @GetMapping("/surname")
+    public String getSurname() {
+        log.info("getSurname() called");
+        return "Kowalski";
     }
 
 }
