9073f5f001e81da78b93fc69a70062b0aaffa8e1
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-05-19 23:22:11 +0200

    IntegrationTest - LAB01

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserRepository.java
@@ -2,6 +2,7 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Objects;
 import java.util.Optional;
