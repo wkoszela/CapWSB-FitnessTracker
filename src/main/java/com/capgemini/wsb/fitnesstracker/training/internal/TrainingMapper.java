f4668a6b8768278940f4f26809976ce5d5428642
piegoose <jedrzej.sikora@puszkin.eu>
2024-11-05 17:29:50 +0100

    TrainingMapper

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
@@ -0,0 +1,12 @@
+package com.capgemini.wsb.fitnesstracker.training.internal;
+
+
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
+
+public class TrainingMapper {
+
+    TrainingDto toDto(Training training) {
+        return new
+    }
+
+}
