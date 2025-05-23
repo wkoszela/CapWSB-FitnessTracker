7c11282c389efd718f1a46eb5723d20a19261dc6
Maciej Dawczak <mac.dawczak@gmail.com>
2025-05-23 22:56:46 +0200

    Add methods for training management

--- src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingRepository.java
+++ src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingRepository.java
@@ -3,6 +3,77 @@ package pl.wsb.fitnesstracker.training.internal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.wsb.fitnesstracker.training.api.Training;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 interface TrainingRepository extends JpaRepository<Training, Long> {
+
+    /**
+     * Finds all available trainings
+     *
+     * @return list of all trainings
+     */
+    default List<Training> findAllTrainings() {
+        return findAll();
+    }
+
+    /**
+     * Finds all trainings for specific user
+     *
+     * @param userId user identifier
+     * @return list of user trainings
+     */
+    default List<Training> findTrainingsByUserId(Long userId) {
+        return findAll().stream()
+                .filter(training -> training.getUser().getId().equals(userId))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Finds all completed trainings after given date
+     *
+     * @param date completion date
+     * @return list of completed trainings
+     */
+    default List<Training> findCompletedTrainingsAfterDate(Date date) {
+        return findAll().stream()
+                .filter(training -> training.getEndTime() != null && training.getEndTime().after(date))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Finds all trainings by activity type
+     *
+     * @param activityType type of activity
+     * @return list of trainings with specified activity
+     */
+    default List<Training> findTrainingsByActivityType(String activityType) {
+        return findAll().stream()
+                .filter(training -> training.getActivityType().name().equalsIgnoreCase(activityType))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Creates new training
+     *
+     * @param training training to create
+     * @return created training
+     */
+    default Training createTraining(Training training) {
+        return save(training);
+    }
+
+    /**
+     * Updates existing training
+     *
+     * @param training training to update
+     * @return updated training
+     */
+    default Training updateTraining(Training training) {
+        return save(training);
+    }
+
 
 }
--- src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingServiceImpl.java
+++ src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingServiceImpl.java
@@ -1,17 +1,53 @@
 package pl.wsb.fitnesstracker.training.internal;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.wsb.fitnesstracker.training.api.Training;
 import pl.wsb.fitnesstracker.training.api.TrainingProvider;
-import pl.wsb.fitnesstracker.user.api.User;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 // TODO: Provide Implementation and correct the return type of the method getTraining
+@Service
+@RequiredArgsConstructor
 public class TrainingServiceImpl implements TrainingProvider {
 
+    private final TrainingRepository trainingRepository;
+
     @Override
-    public Optional<User> getTraining(final Long trainingId) {
-        throw new UnsupportedOperationException("Not finished yet");
+    public Optional<Training> getTraining(final Long trainingId) {
+        return trainingRepository.findById(trainingId);
     }
+
+    public List<Training> getAllTrainings() {
+        return trainingRepository.findAllTrainings();
+    }
+
+    public List<Training> getTrainingsByUserId(Long userId) {
+        return trainingRepository.findTrainingsByUserId(userId);
+    }
+
+    public List<Training> getCompletedTrainingsAfterDate(Date date) {
+        return trainingRepository.findCompletedTrainingsAfterDate(date);
+    }
+
+    public List<Training> getTrainingsByActivityType(String activityType) {
+        return trainingRepository.findTrainingsByActivityType(activityType);
+    }
+
+    public Training createTraining(Training training) {
+        return trainingRepository.createTraining(training);
+    }
+
+    public Training updateTraining(Training training) {
+        return trainingRepository.updateTraining(training);
+    }
+
+
+
+
 
 
 }
