65abc7e12f550f8c0e3483b9500ad6c99aa7a648
Jakub Bielecki <kuba.bielecki@outlook.com>
2025-11-08 15:23:46 +0100

    Fix field name, add comments to nullable fields.

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/HealthMetric.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/HealthMetric.java
@@ -35,27 +35,27 @@ public class HealthMetric {
     @Column(name = "date", nullable = false)
     private Date date;
 
-    @Column(name = "weight")
+    @Column(name = "weight") // moze byc null, poniewaz user moze nie podac
     private Double weight;
 
-    @Column(name = "height")
+    @Column(name = "height") // tak jak wyzej
     private Double height;
 
-    @Column(name = "heart_rate")
-    private Double hearthRate;
+    @Column(name = "heart_rate") // zakladajac ze uzytkownik np nie ma zalozonego zegarka fitness to nie ma jak odczytac tetna
+    private Double heartRate;
 
     public HealthMetric(
         final User user,
         final Date date,
         final Double weight,
         final Double height,
-        final Double hearthRate
+        final Double heartRate
     ) {
 
         this.user = user;
         this.date = date;
         this.weight = weight;
         this.height = height;
-        this.hearthRate = hearthRate;
+        this.heartRate = heartRate;
     }
 }
