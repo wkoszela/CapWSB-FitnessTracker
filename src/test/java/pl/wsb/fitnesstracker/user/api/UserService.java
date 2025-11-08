44a91f071269a9e68461020e92cc37d8c7f074b7
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-11-08 19:00:20 +0100

    test, który nic nie da

---
+++ src/test/java/pl/wsb/fitnesstracker/user/api/UserService.java
@@ -0,0 +1,17 @@
+package pl.wsb.fitnesstracker.user.api;
+
+/**
+ * Interface (API) for modifying operations on {@link User} entities through the API.
+ * Implementing classes are responsible for executing changes within a database transaction, whether by continuing an existing transaction or creating a new one if required.
+ */
+public interface UserService {
+
+    /**
+     * Creates a new user.
+     *
+     * @param user The user to be created
+     * @return The created user
+     */
+    User createUser(User user);
+
+}
---
+++ src/test/java/pl/wsb/fitnesstracker/user/internal/UserRepository.java
@@ -0,0 +1,23 @@
+package pl.wsb.fitnesstracker.user.internal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.util.Objects;
+import java.util.Optional;
+
+interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Query searching users by email address. It matches by exact match.
+     *
+     * @param email email of the user to search
+     * @return {@link Optional} containing found user or {@link Optional#empty()} if none matched
+     */
+    default Optional<User> findByEmail(String email) {
+        return findAll().stream()
+                .filter(user -> Objects.equals(user.getEmail(), email))
+                .findFirst();
+    }
+
+}
