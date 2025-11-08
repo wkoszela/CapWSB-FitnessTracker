acff27828705ce2e6dad48a9a01ce75077617116
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-11-08 19:24:37 +0100

    HealthMetrics unfinished

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -0,0 +1,37 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "total_trainings", nullable = false)
+    private int totalTrainings;
+
+    @Column(name = "total_distance")
+    private double totalDistance;
+
+    @Column(name = "total_calories_burned")
+    private int totalCaloriesBurned;
+
+
+}
