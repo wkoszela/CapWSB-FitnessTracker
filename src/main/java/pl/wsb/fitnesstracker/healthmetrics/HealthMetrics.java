aaef2a5e85cc15588501e8060065be0fb12be7b9
filipwesolowski <filipwesolowski09@gmail.com>
2025-11-08 17:15:01 +0100

    lab 2 - 1 encja

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -10,42 +10,40 @@ import pl.wsb.fitnesstracker.user.api.User;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+
 public class HealthMetrics {
 
-    @Entity
-    @Table(name = "health_metrics")
-    @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @ToString
-    public class HealthMetrics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Nullable
-        private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-        @ManyToOne
-        @JoinColumn (name = "user_id", nullable = false)
-        private User user;
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
 
-        @Column(name = "date", nullable = false)
-        private LocalDate date;
+    @Column(name = "weight")
+    private Double weight;
 
-        @Column(name = "weight")
-        private Double weight;
+    @Column(name = "height")
+    private Integer height;
 
-        @Column(name = "height")
-        private Integer height;
+    @Column(name = "heartRate")
+    private Integer heartRate;
 
-        @Column(name = "heartRate")
-        private Integer HeartRate;
-
-        public HealthMetrics(User user, LocalDate date, Double weight, Integer height, Integer heartRate) {
-            this.user = user;
-            this.date = date;
-            this.weight = weight;
-            this.height = height;
-            this.heartRate = heartRate;
-        }
-
+    public HealthMetrics(User user, LocalDate date, Double weight, Integer height, Integer heartRate) {
+        this.user = user;
+        this.date = date;
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+    }
 }
