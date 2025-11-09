786877a8bf125a70a7d3fe2f3d32bc3b07c72f6c
artoor-b <artur.brzezina0@gmail.com>
2025-11-09 13:50:21 +0100

    add HealthMetrics class

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -0,0 +1,59 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+        import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Health_Metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(nullable = false)
+    private Float weight;
+
+    @Column(nullable = false)
+    private Float height;
+
+    @Column(nullable = false)
+    private Float heartRate;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    public HealthMetrics(
+            final LocalDate date,
+            final Float weight,
+            final Float height,
+            final Float heartRate) {
+
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+        this.date = date;
+    }
+
+}
+
+
