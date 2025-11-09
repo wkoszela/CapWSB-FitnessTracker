da2b83fcaec65826a2b4443298532640d2e91ca6
Tubis <TubisMaciej@gmail.com>
2025-11-09 11:23:51 +0100

    6/6 tests passed

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -1,7 +1,43 @@
 package pl.wsb.fitnesstracker.healthmetrics;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.wsb.fitnesstracker.user.api.User;
+import java.time.LocalDate;
 
+
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+
+    @Column(name = "weight")
+    private Double weight;
+
+
+    @Column(name = "height")
+    private Double height;
+
+
+    @Column(name = "heartRate")
+    private Integer heartRate;
 }
