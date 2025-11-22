bc1e4f9e0d23d4e58e44dc474b5c0b5ded3da830
Krzysztof Maciejewski <krzysztofmaciejewski16@gmail.com>
2025-11-22 15:23:02 +0100

    zmiana pliku WorkoutSession.java

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,58 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import pl.wsb.fitnesstracker.training.api.Training; // Import encji Training
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date; // Używamy Date dla spójności z Training
+
+@Entity
+@Table(name = "workout_session") // Wymagane przez test
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Zmieniamy int na Long (standard w JPA)
+
+    // Zamiast 'int trainingId', robimy relację do obiektu Training
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "training_id", nullable = false) // To stworzy kolumnę 'training_id'
+    private Training training;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp; // Zmieniamy String na Date
+
+    @Column(name = "start_latitude")
     private double startLatitude;
+
+    @Column(name = "start_longitude")
     private double startLongitude;
+
+    @Column(name = "end_latitude")
     private double endLatitude;
+
+    @Column(name = "end_longitude")
     private double endLongitude;
+
+    @Column(name = "altitude")
     private double altitude;
+
+    // Konstruktor
+    public WorkoutSession(Training training, Date timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude) {
+        this.training = training;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
 }
