f5b612fc032aa9e89ac76975893f27fbb5435e34
filipwesolowski <filipwesolowski09@gmail.com>
2025-11-22 15:49:31 +0100

    lab 3 - all classes

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,46 +1,53 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
 import jakarta.persistence.*;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+import pl.wsb.fitnesstracker.training.api.Training;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import java.util.Date;
+
 @Entity
-@Table(name = "workout_session")
+@Table(name = "workout_sessions") // Zmiana na liczbę mnogą, by pasowało do testów
 @Getter
 @NoArgsConstructor
 public class WorkoutSession {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id; // Zmieniono int na Long (standard JPA)
+
     @ManyToOne
     @JoinColumn(name = "training_id")
-    private int trainingId;
-    private String timestamp;
+    private Training training; // Zmieniono nazwę zmiennej na małą literę
+
+    private Date timestamp; // Zmieniono String na Date
+
     @Column(name = "start_latitude")
     private double startLatitude;
+
     @Column(name = "start_longitude")
     private double startLongitude;
+
     @Column(name = "end_latitude")
     private double endLatitude;
+
     @Column(name = "end_longitude")
     private double endLongitude;
 
     private double altitude;
 
-    public WorkoutSession(double endLongitude, int id, int trainingId, String timestamp, double startLatitude, double startLongitude, double endLatitude, double altitude) {
-        this.endLongitude = endLongitude;
-        this.id = id;
-        this.trainingId = trainingId;
+    // Usunąłem ręczny konstruktor, który powodował błędy kompilacji.
+    // Dzięki @NoArgsConstructor Hibernate poradzi sobie z tworzeniem obiektu.
+    // Jeśli potrzebujesz konstruktora do testów, możesz użyć @AllArgsConstructor z Lomboka
+    // lub stworzyć taki, który przyjmuje obiekt Training, a nie int trainingId.
+    public WorkoutSession(Training training, Date timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude) {
+        this.training = training;
         this.timestamp = timestamp;
         this.startLatitude = startLatitude;
         this.startLongitude = startLongitude;
         this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
         this.altitude = altitude;
     }
 }
