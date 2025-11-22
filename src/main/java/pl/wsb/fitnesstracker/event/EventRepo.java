25dfa58af1780295b5d5da6a213042fe4d8ffef5
Maciej Tubis <TubisMaciej@gmail.com>
2025-11-22 15:38:57 +0100

    Configured EventRepo

--- src/main/java/pl/wsb/fitnesstracker/event/EventRepo.java
+++ src/main/java/pl/wsb/fitnesstracker/event/EventRepo.java
@@ -1,4 +1,27 @@
 package pl.wsb.fitnesstracker.event;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import pl.wsb.fitnesstracker.entity.Event;
+
+import java.util.List;
+
+@Repository
 public class EventRepo {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public List<Event> findByName(String name) {
+        String jpql = "SELECT * FROM Event WHERE Event.name = :eventName";
+
+        TypedQuery<Event> query = entityManager.createQuery(jpql, Event.class);
+
+        query.setParameter("eventName", name);
+
+        return query.getResultList();
+    }
 }
