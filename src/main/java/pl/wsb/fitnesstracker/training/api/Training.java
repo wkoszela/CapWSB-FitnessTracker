ad758a2196f05ae845d86339b8967b4fe7ce305e
filipwesolowski <filipwesolowski09@gmail.com>
2025-11-22 15:20:03 +0100

    lab 3 - training.java

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,26 +1,44 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
 
+import javax.naming.Name;
 import java.util.Date;
 
+@Entity
+@Table(name = "trainings")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "start_time")
     private Date startTime;
 
+    @Column(name = "end_time")
     private Date endTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "activity_type")
     private ActivityType activityType;
 
     private double distance;
 
+    @Column(name = "average_speed")
     private double averageSpeed;
 
     public Training(
