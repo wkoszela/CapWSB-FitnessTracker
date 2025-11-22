d96b44916f9d9092fd3593e593f09f1161679a82
filipwesolowski <filipwesolowski09@gmail.com>
2025-11-22 15:39:18 +0100

    lab 3 - all classes

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,33 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "events")
+@Getter
+@NoArgsConstructor
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private String city;
+
+    private String country;
+
+    @Column(name = "start_time")
+    private Date startTime;
+
+    @Column(name = "end_time")
+    private Date endTime;
 }
--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,46 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
+import jakarta.persistence.*;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 // TODO: Define the Event entity with appropriate fields and annotations
+@Entity
+@Table(name = "workout_session")
+@Getter
+@NoArgsConstructor
 public class WorkoutSession {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @ManyToOne
+    @JoinColumn(name = "training_id")
     private int trainingId;
     private String timestamp;
+    @Column(name = "start_latitude")
     private double startLatitude;
+    @Column(name = "start_longitude")
     private double startLongitude;
+    @Column(name = "end_latitude")
     private double endLatitude;
+    @Column(name = "end_longitude")
     private double endLongitude;
+
     private double altitude;
+
+    public WorkoutSession(double endLongitude, int id, int trainingId, String timestamp, double startLatitude, double startLongitude, double endLatitude, double altitude) {
+        this.endLongitude = endLongitude;
+        this.id = id;
+        this.trainingId = trainingId;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.altitude = altitude;
+    }
 }
