85121de5c29f46752dfe7f3437e5746b4c2d9aa6
Marcin Kwiatkowski <contact@marcinkwiatkowski.com>
2025-11-23 14:57:20 +0100

    added training

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,32 +1,58 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
+import pl.wsb.fitnesstracker.workoutsession.WorkoutSession;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "trainings")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    private Date startTime;
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
 
-    private Date endTime;
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "activity_type", nullable = false)
     private ActivityType activityType;
 
+    @Column(nullable = false)
     private double distance;
 
+    @Column(name = "average_speed", nullable = false)
     private double averageSpeed;
+
+    @OneToMany(mappedBy = "training", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkoutSession> workoutSessions = new ArrayList<>();
 
     public Training(
             final User user,
-            final Date startTime,
-            final Date endTime,
+            final LocalDateTime startTime,
+            final LocalDateTime endTime,
             final ActivityType activityType,
             final double distance,
             final double averageSpeed) {
