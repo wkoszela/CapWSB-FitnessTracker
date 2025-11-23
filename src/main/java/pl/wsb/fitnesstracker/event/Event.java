dd82a1ef7fd916529325007f02a4fe1124d4841a
AdamPar <adamparkitny7@gmail.com>
2025-11-23 19:43:14 +0100

    feat: update event entity

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,57 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT", nullable = true)
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = true)
+    private LocalDateTime endTime;
+
+    @Column(name = "country", nullable = true)
+    private String country;
+
+    @Column(name = "city", nullable = true)
+    private String city;
+
+    public Event(
+            final String name,
+            final String description,
+            final LocalDateTime startTime,
+            final LocalDateTime endTime,
+            final String country,
+            final String city) {
+
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
