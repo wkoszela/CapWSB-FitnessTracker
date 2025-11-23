856ce51199222f0cbfa277be5fee67b527dda395
Marcin Kwiatkowski <contact@marcinkwiatkowski.com>
2025-11-23 14:54:24 +0100

    feat: added UserEvent

---
+++ src/main/java/pl/wsb/fitnesstracker/event/UserEvent.java
@@ -0,0 +1,43 @@
+package pl.wsb.fitnesstracker.event;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+@Entity
+@Table(name = "user_event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class UserEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(nullable = false)
+    private String status;
+
+    public UserEvent(
+            final User user,
+            final Event event,
+            final String status) {
+        this.user = user;
+        this.event = event;
+        this.status = status;
+    }
+}
+
