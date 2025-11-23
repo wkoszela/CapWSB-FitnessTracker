37684173f2dc85ba4a86418f01287f4753549d70
Marcin Kwiatkowski <contact@marcinkwiatkowski.com>
2025-11-23 14:53:14 +0100

    feat: added event entity

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,56 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    public Event(
+            final String name,
+            final String description,
+            final LocalDateTime startTime,
+            final LocalDateTime endTime,
+            final String country,
+            final String city) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
