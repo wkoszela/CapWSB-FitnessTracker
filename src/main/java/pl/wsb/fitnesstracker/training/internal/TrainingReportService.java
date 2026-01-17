047c77b383249a0f475663f3f022ba55c6ce8273
Krzysztof Maciejewski <krzysztofmaciejewski16@gmail.com>
2026-01-17 16:44:06 +0100

    LAB06 - zadanie 2

--- src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingReportService.java
+++ src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingReportService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pl.wsb.fitnesstracker.mail.api.EmailDto;
+import pl.wsb.fitnesstracker.mail.api.EmailSender;
 import pl.wsb.fitnesstracker.training.api.Training;
 import pl.wsb.fitnesstracker.training.api.TrainingProvider;
 import pl.wsb.fitnesstracker.user.api.User;
@@ -21,6 +23,7 @@ class TrainingReportService {
 
     private final TrainingProvider trainingProvider;
     private final UserProvider userProvider;
+    private final EmailSender emailSender;
 
     @Scheduled(cron = "0 0 0 * * MON")
     public void generateWeeklyReport() {
@@ -38,6 +41,14 @@ class TrainingReportService {
                     user.getLastName(), 
                     user.getId(), 
                     trainings.size());
+            
+            String subject = "Weekly Training Report";
+            String content = "Hello " + user.getFirstName() + ",\n\n" +
+                             "You have completed " + trainings.size() + " trainings in the last week.\n\n" +
+                             "Keep up the good work!\n" +
+                             "FitnessTracker Team";
+                             
+            emailSender.send(new EmailDto(user.getEmail(), subject, content));
         }
         
         log.info("Weekly training report generation finished.");
