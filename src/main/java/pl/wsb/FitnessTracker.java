6571476d6e0e927f5b5c928f29ae44b72b88d9d9
Krzysztof Maciejewski <krzysztofmaciejewski16@gmail.com>
2026-01-17 17:11:25 +0100

    LAB06 - zadanie 2

--- src/main/java/pl/wsb/FitnessTracker.java
+++ src/main/java/pl/wsb/FitnessTracker.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = "pl.wsb.fitnesstracker")
 @EnableJpaRepositories(basePackages = "pl.wsb.fitnesstracker")
 @SpringBootApplication
+@EnableScheduling
 public class FitnessTracker {
 
     public static void main(String[] args) {
